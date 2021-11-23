1a94d892eeea85bc868de72fb30dad171bd1c02c
KhangNguyen <khang.nguyen1@ntq-solution.com.vn>
2021-11-23 16:03:58 +0700

    add commit

--- src/main/java/com/example/selfgeneratedtextid/MyGenerator.java
+++ src/main/java/com/example/selfgeneratedtextid/MyGenerator.java
@@ -12,8 +12,12 @@ public class MyGenerator implements IdentifierGenerator {
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object obj) throws HibernateException {
         String query = "SELECT u.id FROM User u";
-        Stream<String> ids = session.createQuery(query, String.class).stream();
+        Stream<String> ids = session.createQuery(query, String.class).stream();// Thực hiện câu lệnh , lấy dc danh sách id
+        // sau đó chuyển sang Stream => ids h là 1 stream
         Long max = ids.map(o -> o.replace(prefix, "")).mapToLong(Long::parseLong).max().orElse(0L);
+        // .map thay thế prefix thành chuỗi rỗng
+        // do đang là String nên mapToLong chuyển sang Long , max() lấy giá trị lớn nhất , nếu ko có trả về 0L
         return prefix + (String.format("%04d", max + 1));
+        // trả về id dạng String 
     }
 }
